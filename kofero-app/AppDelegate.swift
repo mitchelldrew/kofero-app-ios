d2f47110ab386d6b717da26dda2b0fb11ac6b037
mitchdrew <mitchdrew@goatse.cx>
2021-03-09 09:31:11 -0800

    develop - mdrew: testing no storybord

--- kofero-app/AppDelegate.swift
+++ kofero-app/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import NeedleFoundation
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        registerProviderFactories()
         // Override point for customization after application launch.
         return true
     }
